44d7a76583be884af0d6c0aeeeddedb1affcfd0b
Ngo Dang Tan <ngodangtan1994@gmail.com>
2021-01-22 10:25:05 +0700

    first commit

---
+++ SwiftUIGauge WatchKit Extension/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SwiftUIGauge WatchKit Extension
+//
+//  Created by Ngo Dang tan on 22/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var gradient = Gradient(colors: [.blue, .pink ,.purple ,.orange])
+    var body: some View {
+       
+        Gauge(value: 33.0, in: 0.0...100.0){
+            Text("Speed")
+                .foregroundColor(Color.blue)
+        } currentValueLabel: {
+            Text("33")
+                .foregroundColor(Color.blue)
+        } minimumValueLabel: {
+            Text("0")
+                .foregroundColor(Color.blue)
+        } maximumValueLabel: {
+            Text("100")
+                .foregroundColor(Color.blue)
+        }
+        .gaugeStyle(CircularGaugeStyle(tint: gradient))
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGauge WatchKit Extension/SwiftUIGaugeApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIGaugeApp.swift
+//  SwiftUIGauge WatchKit Extension
+//
+//  Created by Ngo Dang tan on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
